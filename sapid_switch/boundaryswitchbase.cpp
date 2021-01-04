6bf5a9f36befb7259450853dc8651123277b007b
MRXY001 <wxy19980615@gmail.com>
2021-01-04 16:42:33 +0800

    重构长矩形的动画逻辑，使用真正的线条长度计算方式

--- sapid_switch/boundaryswitchbase.cpp
+++ sapid_switch/boundaryswitchbase.cpp
@@ -4,6 +4,7 @@ BoundarySwitchBase::BoundarySwitchBase(QWidget *parent) : SapidSwitchBase(parent
 {
     setMinimumSize(128, 32);
 
+    setBackground(QColor(30, 144, 255), Qt::gray);
     setBorder(QColor(30, 144, 255), 1);
     setAnimationDuration(600);
     setAnimationEasingCurve(QEasingCurve::OutCubic);
@@ -38,7 +39,7 @@ QSize BoundarySwitchBase::sizeHint() const
 
 void BoundarySwitchBase::calculateGeometry()
 {
-    radius = (height() - 1) / 2;
+    radius = height() / 2;
 }
 
 QPainterPath BoundarySwitchBase::getBgPath() const
@@ -87,7 +88,6 @@ void BoundarySwitchBase::drawBorder(QPainter &painter)
             painter.setPen(colorOff);
             painter.drawPath(path);
             painter.restore();
-//            painter.drawPath(clipPath);
         }
 
         // 画ON
@@ -115,9 +115,139 @@ void BoundarySwitchBase::drawBorder(QPainter &painter)
             painter.setClipPath(clipPath);
             painter.setPen(colorOn);
             painter.drawPath(path);
-//            painter.drawPath(clipPath);
             painter.restore();
         }
+    }
+    else if (mode == 2)
+    {
+        painter.save();
+        QRect rect = this->rect();
+        painter.setPen(colorOff);
+        QPainterPath path;
+        path.addRoundedRect(rect, radius, radius);
+        painter.drawPath(path);
+
+        // 画动画背景
+        const double totalLen = 2 * PI * radius + 2 * (rect.width() - 2 * radius);
+        double currLen = totalLen * prop; // 应当动画的时长
+        painter.setPen(colorOn);
+        path.clear();
+        path.moveTo(0, radius);
+        if (currLen > 0) // 左下角四分之一个圆
+        {
+            QPoint o(rect.left() + radius, radius);
+            double len = radius * PI / 2; // 90°周长
+            double angle;
+            if (len <= currLen) // 够长
+            {
+                angle = 90;
+                currLen -= len;
+            }
+            else // len > currLen，不够这一个圆的
+            {
+                angle = 90 * currLen / len;
+                currLen = 0;
+            }
+            path.arcTo(QRect(0, 0, radius*2, radius*2), 180, angle);
+        }
+        if (currLen > 0) // 下面直线
+        {
+            double len = rect.width() - 2 * radius;
+            double moveLen;
+            if (currLen > len) // 够长
+            {
+                moveLen = len;
+                currLen -= moveLen;
+            }
+            else
+            {
+                moveLen = currLen;
+                currLen = 0;
+            }
+            path.lineTo(radius + moveLen, radius*2);
+        }
+        if (currLen > 0) // 右边半圆
+        {
+            QPoint o(rect.right() - radius, radius);
+            double len = radius * PI; // 半圆周长
+            double angle;
+            if (len <= currLen) // 够长
+            {
+                angle = 180;
+                currLen -= len;
+            }
+            else // len > currLen，不够这一个圆的
+            {
+                angle = 180 * currLen / len;
+                currLen = 0;
+            }
+            path.arcTo(QRect(width()-2*radius, 0, radius*2, radius*2), 270, angle);
+        }
+        if (currLen > 0) // 上面直线
+        {
+            double len = rect.width() - 2 * radius;
+            double moveLen;
+            if (currLen > len) // 够长
+            {
+                moveLen = len;
+                currLen -= moveLen;
+            }
+            else
+            {
+                moveLen = currLen;
+                currLen = 0;
+            }
+            path.lineTo(width()-radius-moveLen, 0);
+        }
+        if (currLen > 0) // 左上角圆
+        {
+            QPoint o(rect.left() + radius, radius);
+            double len = radius * PI / 2; // 90°周长
+            double angle;
+            if (len <= currLen) // 够长
+            {
+                angle = 90;
+                currLen -= len;
+            }
+            else // len > currLen，不够这一个圆的
+            {
+                angle = 90 * currLen / len;
+                currLen = 0;
+            }
+            path.arcTo(QRect(0, 0, radius*2, radius*2), 90, angle);
+        }
+        painter.setPen(colorOn);
+        painter.drawPath(path);
+        painter.restore();
+
+        /*painter.save();
+        // 右半部分整体rect
+        QPainterPath path;
+        QRect rect = this->rect();
+        path.addRoundedRect(rect, radius, radius);
+        double startAngle = 180;
+        double arcAngle = 360 * prop / 0.75;
+
+        // 去掉的部分
+        QPainterPath clipPath;
+        const QPoint center = rect.center();
+        const int r = rect.width()/2;
+        clipPath.moveTo(center);
+        clipPath.lineTo(this->rect().center());
+        QRect circleRect(center.x()-r-1, center.y()-r, r*2+2, r*2);
+        clipPath.arcTo(circleRect, startAngle, arcAngle);
+        clipPath.lineTo(center);
+
+        painter.setPen(colorOff);
+        painter.drawPath(path);
+        painter.setClipPath(clipPath);
+        painter.setPen(colorOn);
+        painter.drawPath(path);
+        painter.restore();*/
+    }
+    else if (mode == 3)
+    {
+
     }
 }
 
